3bd8a7b4a58687d5bf6b06208b0dfe5aaf9d91bb
miguel <miguel@Miguels-MacBook-Air.local>
2022-11-28 11:31:54 -0600

    ajuste opciones cors

--- server/index.js
+++ server/index.js
@@ -14,11 +14,12 @@ const corsOptions ={
 }
 
 
-app.use(cors(corsOptions));//Permite comunicar servidor front-end y servidor back-end
+
 app.use((req, res, next) => {
   res.header({"Access-Control-Allow-Origin": "*"});
   next();
 }) 
+app.use(cors(corsOptions));//Permite comunicar servidor front-end y servidor back-end
 app.use(express.json());
 
 
