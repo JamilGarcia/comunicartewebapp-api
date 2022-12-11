e562f95ea91bda490de35e51b2dd6fe23b6f7c06
miguel <miguel@Miguels-MacBook-Air.local>
2022-12-10 19:20:02 -0600

    Inclusion de rutas para obtener nombre de usuario para menu

--- server/routes/tasks.route.js
+++ server/routes/tasks.route.js
@@ -6,5 +6,9 @@ const router = require('express').Router();
 router.post("/", verifyUser);
 router.get("/test", testUser);
 router.post("/perfil_usuario",getDatosUsuario);
+router.post("/Gerente", ObtenerNombreUsuario);
+router.post("/Subgerente", ObtenerNombreUsuario);
+router.post("/modificar_perfil", getDatosUsuario);
+router.post("/Ejecutivo_cuenta", ObtenerNombreUsuario); 
 
 module.exports = router;
